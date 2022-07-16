07137f3719e1442e5261d5c92452f3ea5434252c
Enzo Falcon <falcone134@gmail.com>
2022-07-16 18:09:37 -0500

    Adding Post model to admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from projects.models import Post
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created')
+    date_hierarchy = 'created'
+
+admin.site.register(Post, PostAdmin)
--- projects/models.py
+++ projects/models.py
@@ -9,3 +9,6 @@ class Post(models.Model):
     created = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now=True)
     body = models.TextField()
+
+    def __str__(self):
+        return self.title
