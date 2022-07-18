4179739ce1e3730096c81cb6a8d8a8979478df4a
Enzo Falcon <falcone134@gmail.com>
2022-07-18 14:45:38 -0500

    Delete unusable project

--- projects/serializers.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import fields
-from rest_framework import serializers
-from projects.models import Project
-
-class ProjectSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Project
-        fields = '__all__'
--- projects/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import include, path
-from projects import views
-
-urlpatterns = [
-    path('posts/', views.ListPost.as_view(), name='posts-list'),
-]
