a3a22c104fb7f66681f01a5500a58115416beed5
Enzo Falcon <falcone134@gmail.com>
2022-07-19 12:53:33 -0500

    Fixing post summary api in urls

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'projects', views.ProjectViewSet, basename="projects")
-router.register(r'posts-summary', views.ListRetrievePostSummary, basename="posts-summary")
+router.register(r'posts-summary', views.ListPostSummary, basename="posts-summary")
 router.register(r'posts', views.RetrieveCreateUpdateDeletePost, basename="posts")
 
 urlpatterns = [
