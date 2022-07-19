eaad0dc06dbec2893ddd8d27007e7c0fa82c1019
Enzo Falcon <falcone134@gmail.com>
2022-07-19 12:22:32 -0500

    Comments in Project and Post summary serializer

--- api/serializers.py
+++ api/serializers.py
@@ -2,12 +2,19 @@ from rest_framework import serializers
 from api.models import Project, Post
 
 class ProjectSerializer(serializers.ModelSerializer):
+    """
+    Serializing the entire Project model.
+    """
 
     class Meta:
         model = Project
         fields = '__all__'
 
 class ResumePostSerializer(serializers.ModelSerializer):
+    """
+    Serializing just some fields from Post serializer because this is used
+    when we want to show just a post summary.
+    """
 
     class Meta:
         model = Post
