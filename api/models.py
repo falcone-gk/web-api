fac8ccf78bfd556c9f50d5866368bb5d7ed02f98
Enzo Falcon <falcone134@gmail.com>
2022-07-19 16:37:32 -0500

    Add migrations for tag new field in model

--- api/models.py
+++ api/models.py
@@ -14,6 +14,7 @@ class Project(models.Model):
     title = models.CharField(max_length=60)
     description = models.TextField()
     created = models.DateField(auto_now_add=True)
+    tag = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.title
